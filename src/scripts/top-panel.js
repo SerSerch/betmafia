a2f3bf7838a5cdf290bd12fc396ea4269d39d5a3
Vladimir Yazykov <neizerth@gmail.com>
2018-05-24 13:20:28 +0300

    исправление панельки на mobile

--- src/scripts/top-panel.js
+++ src/scripts/top-panel.js
@@ -14,8 +14,9 @@ const close = () => menuCheckbox.checked = false,
       menu = document.querySelector( '.menu' ),
       onclick = e => hasClass( e.target, 'menu__section' ) && close(),
       onscroll = e => {
-        const top = window.pageYOffset;
-        top < scrollTop ? addClass( panel, 'top-panel_active' ) : removeClass( panel, 'top-panel_active' );
+        const top = window.pageYOffset,
+          show = top < 100 || top < scrollTop;
+        show ? addClass( panel, 'top-panel_active' ) : removeClass( panel, 'top-panel_active' );
         scrollTop = top;
       };
     on( menu, 'click', onclick );
